b1f1f5dead1ae1f1514dc0cda9848e7207e5b324
Daniel D Demerin <ddemerin@gmail.com>
2020-05-11 14:47:13 -0400

    restarting cohort assignments from the beginning

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace variables_v2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbersOfCupsOfCoffee = 4;
+            var fullName = "Daniel Dean Demerin";
+            var today = DateTime.Now;
+
+        }
+    }
+}
